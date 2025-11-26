36fb9e936e1a1d9906fbff7099911f6b1776119b
Елена Сажина <Cube.89@list.ru>
2025-11-26 19:32:45 +0300

    Добавление шаблонов файлов, стилей и ссылки для слабовидящих

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Сайт работает!"
+    return render_template("index.html", vision_mode=False)
+
+@app.route("/vision")
+def vision():
+    return render_template("index.html", vision_mode=True)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html", vision_mode=False), 404
+
+@app.route("/sitemap")
+def sitemap():
+    return render_template("sitemap.html", vision_mode=False)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
